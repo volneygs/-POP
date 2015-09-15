83f326974065b45b7e2435701e7f6bd658964713
WillZork <williamberg.ferreira@ccc.ufcg.edu.br>
2015-09-15 16:27:56 -0300

    Implementado conversor de datas, exceptions de datas no cadastro e correções na tradução do script usecase 1.

--- src/users/User.java
+++ src/users/User.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 
 import factory.PostFactory;
 import java.time.*;
+import java.time.format.*;
 
 public class User {
 	
+	private DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	private String email;
 	private String name;
 	private String password;
-	private String birthdate;
+	private LocalDate birthdate;
 	private String image;
 	private String pop;
 	private ArrayList<Post> mural;
@@ -35,10 +37,22 @@ public class User {
 		}
 
 		
+		try { 
+			this.birthdate = LocalDate.parse(birthdate, dateFormat);
+		
+		} catch (DateTimeException e) {
+						
+			if (e.toString().contains("could not be parsed at index")){
+				throw new Exception("Error: Invalid date format.");
+			
+			}else if (e.toString().contains("Invalid value for")){
+				throw new Exception("Error: Date doesn't exist.");
+			}
+		}
+		
 		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.birthdate = birthdate;
 		this.image = "default_image.png";
 		this.mural = new ArrayList<Post>();
 		this.friends = new ArrayList<User>();
@@ -63,10 +77,22 @@ public class User {
 		}
 
 		
+		try { 
+			this.birthdate = LocalDate.parse(birthdate, dateFormat);
+		
+		} catch (DateTimeException e) {
+						
+			if (e.toString().contains("could not be parsed at index")){
+				throw new Exception("Error: Invalid date format.");
+			
+			}else if (e.toString().contains("Invalid value for")){
+				throw new Exception("Error: Date doesn't exist.");
+			}
+		}
+		
 		this.email = email;
 		this.name = name;
 		this.password = password;
-		this.birthdate = birthdate;
 		this.image = image;
 		this.mural = new ArrayList<Post>();
 		this.friends = new ArrayList<User>();
